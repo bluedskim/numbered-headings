aab47ff09d322e9281845c0449b5890261099fbe
dennis <bluedskim@gmail.com>
2025-04-21 20:43:10 +0900

    chore(package): rename output zip file to reflect version
    Changed the name of the output zip file in package.sh from package.zip to numbered-headngs-v1.0.zip to indicate the current version of the package. This helps in tracking specific builds and ensuring that the correct files are distributed.

--- package.sh
+++ package.sh
@@ -1,1 +1,1 @@
-zip package.zip background.js manifest.json typo--h1-16x16.png typo--h1-48x48.png typo--h1-128x128.png
+zip numbered-headngs-v1.0.zip background.js manifest.json typo--h1-16x16.png typo--h1-48x48.png typo--h1-128x128.png
